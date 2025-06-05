357e9979f616e9922a39591ecb1075f85bc543c0
nnamdi <uzonnamdi31@gmail.com>
2025-06-05 17:17:45 +0100

    colour change

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,17 +3,17 @@ import { ExternalLink, Github } from "lucide-react";
 
 export default function Card({ img, name, desc, stack, link, gitlink }) {
     return (
-        <div className="bg-white shadow-md rounded-lg overflow-hidden min-h-[400px] flex flex-col">
+        <div className="bg-white dark:bg-[#1e293b] shadow-md rounded-lg overflow-hidden min-h-[400px] flex flex-col">
             <img
                 src={img}
                 alt=""
-                className="border-b border-gray-300 h-[200px] w-full object-cover"
+                className="border-b border-gray-300 dark:border-none h-[200px] w-full object-cover"
             />
 
             <div className="flex flex-col justify-between flex-1 p-4">
                 <div>
-                    <h4 className="text-xl font-semibold">{name}</h4>
-                    <p className="mt-2 text-gray-500 text-sm">{desc}</p>
+                    <h4 className="text-xl font-semibold dark:text-gray-200">{name}</h4>
+                    <p className="mt-2 text-gray-500 dark:text-gray-400 text-sm">{desc}</p>
 
                     <div className="mt-3 flex flex-wrap gap-2">
                         {stack?.map((item, index) => (
@@ -31,14 +31,14 @@ export default function Card({ img, name, desc, stack, link, gitlink }) {
                     <a
                         href={link}
                         target="_blank"
-                        className="flex items-center gap-2 border border-gray-300 px-3 py-2 text-sm hover:bg-gray-100 transition rounded-md"
+                        className="flex items-center gap-2 dark:text-gray-300 border border-gray-300 px-3 py-2 text-sm hover:bg-gray-100 transition rounded-md"
                     >
                       Demo <ExternalLink size={16} />
                     </a>
                     <a
                         href={gitlink}
                         target="_blank"
-                        className="flex items-center gap-2 border border-gray-300 px-3 py-2 text-sm hover:bg-gray-100 transition rounded-md"
+                        className="flex items-center gap-2 dark:text-gray-300 border border-gray-300 px-3 py-2 text-sm hover:bg-gray-100 transition rounded-md"
                     >
                         Github <Github size={16} />
                     </a>
